7fb837a3f59b685dec1ea5e869705d9bbf53074d
michyking <michyking1@gmail.com>
2024-04-09 14:15:24 +0100

    nav bar

---
+++ js/nav.js
@@ -0,0 +1,23 @@
+ // FOR THE MOBILE MENU ICONS
+ const menuIcon = document.getElementById('menu-icon');
+
+ 
+ function menuIconClass() {
+   menuIcon.classList.toggle('fa-bars-staggered');
+   menuIcon.classList.toggle('fa-xmark');
+ }
+ menuIcon.addEventListener('click', function() {
+   menuIconClass();
+ });
+
+//  FOR THE ACCOUNT CARET
+const caretIcon = document.getElementById('caret-icon');
+const account = document.getElementById('account');
+ 
+ function caretIconClass() {
+     caretIcon.classList.toggle('fa-caret-down');
+   caretIcon.classList.toggle('fa-caret-up');
+ }
+ account.addEventListener('mouseover', function() {
+   caretIconClass();
+ });
