bae140e2d63f5f72086dad015aaaed9c948b93b7
michyking <michyking1@gmail.com>
2024-04-15 15:43:48 +0100

    functionality added

--- js/register.js
+++ js/register.js
@@ -3,10 +3,39 @@ const lastName = document.getElementById('lname');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('conpassword');
-const btn = document.getElementById('btn');
+// const btn = document.getElementById('btn');
+const form = document.getElementById('form');
 
-btn.addEventListener('click', (e)=>{
-    e.preventDefault()
-    console.log('i have been clicked');
-
+form.addEventListener('submit', (e)=>{
+  e.preventDefault()
+  checkInputs();
 })
+
+const checkInputs = ()=>{
+
+  // Value of each input
+
+  const firstNameValue =  firstName.value.trim()
+
+  if(firstNameValue === ''){
+    setErrorFor(firstName, 'First name cannot blank')
+  }else{
+    setSuccessFor(firstName)
+  }
+}
+
+function setErrorFor(input, message) {
+  const inputcontainer = input.parentElement;
+  const small = inputcontainer.querySelector('small')
+
+  // add error message inside small tag
+  small.innerText = message;
+
+  // add error class
+  inputcontainer.className = "input-holder error";
+}
+
+function setSuccessFor(input) {
+  const inputcontainer = input.parentElement;
+  inputcontainer.className = 'inputcontainer success';
+}
