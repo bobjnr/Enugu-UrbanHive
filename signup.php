17216233fa0b6449c7094e25f5d4a9e772f1c80d
michyking <michyking1@gmail.com>
2024-04-29 20:38:09 +0100

    little changes

--- signup.php
+++ signup.php
@@ -21,14 +21,7 @@
         <h1>
           <a href="index.html">Enugu-Urban<span>Hive</span></a>
         </h1>
-        <div class="search">
-          <input
-            type="search"
-            name=""
-            id=""
-            placeholder="Search Products"
-          /><i class="fa-solid fa-magnifying-glass"></i>
-        </div>
+       
         <nav>
           <div class="nav-items">
             <div class="dropdown">
@@ -41,8 +34,8 @@
               </div>
             </div>
             <div>
-              <i class="fa-solid fa-cart-shopping"></i>
-              <span class="item-counter">0</span>
+              <i class="fa-solid fa-cart-shopping" id="cart"></i>
+              <span class="item-counter" id="counter">0</span>
             </div>
           </div>
         </nav>
@@ -57,34 +50,16 @@
           <div class="mobile-nav-account">
             <p>Enugu-UrbanHive Account</p>
             <div class="nav-account-links">
-              <a href=""><i class="fa-solid fa-right-to-bracket"></i>Login</a>
-              <a href="register.php"
-                ><i class="fa-solid fa-address-card"></i>Register</a
-              >
+              <a href="login.php"><i class="fa-solid fa-right-to-bracket"></i>Login</a>
+              <a href="register.php"><i class="fa-solid fa-address-card"></i>Register</a>
             </div>
             <hr />
             <div class="mobile-nav-categories">
               <p>Our Categories</p>
-              <a href=""><i class="fa-solid fa-mobile"></i>smartphones</a>
-              <a href=""><i class="fa-solid fa-laptop"></i>laptops</a>
-              <a href=""><i class="fa-solid fa-check-double"></i>fragrances</a>
-              <a href=""><i class="fa-solid fa-check-double"></i>skincare</a>
-              <a href=""
-                ><i class="fa-solid fa-basket-shopping"></i>groceries</a
-              >
-              <a href=""><i class="fa-solid fa-chair"></i>home-decoration</a>
-              <a href=""><i class="fa-solid fa-couch"></i>furniture</a>
-              <a href=""><i class="fa-solid fa-person"></i>womens-dresses</a>
-              <a href=""><i class="fa-solid fa-shoe-prints"></i>womens-shoes</a>
-              <a href=""><i class="fa-solid fa-person"></i>mens-shirts</a>
-              <a href=""><i class="fa-solid fa-shoe-prints"></i>mens-shoes</a>
-              <a href=""><i class="fa-solid fa-stopwatch"></i>mens-watches</a>
-              <a href=""><i class="fa-solid fa-stopwatch"></i>womens-watches</a>
-              <a href=""><i class="fa-solid fa-bag-shopping"></i>womens-bags</a>
-              <a href=""><i class="fa-solid fa-ring"></i>womens-jewellery</a>
-              <a href=""><i class="fa-solid fa-check-double"></i>sunglasses</a>
-              <a href=""><i class="fa-solid fa-car"></i>automotive</a>
-              <a href=""><i class="fa-solid fa-motorcycle"></i>motorcycle</a>
+          
+              <a href=""><i class="fa-solid fa-person"></i>women Wears</a>
+              <a href=""><i class="fa-solid fa-person"></i>Men Wears</a>
+             
             </div>
             <hr />
             <div class="mobile-nav-basic">
@@ -93,6 +68,18 @@
             </div>
           </div>
         </div>
+      </div>
+
+      <!-- THE CART -->
+
+      <div class="cart-summary">
+        <div class="cart-summary-head"><p>My Cart</p></div>
+        <div id="cartItem">Your cart is empty</div>
+        <div class="cart-summary-foot">
+            <h4>Total</h4>
+            <h4 id="total"># 0.00</h4>
+        </div>
+        <button class="cart-summary-btn">Checkout</button>
       </div>
     </header>
 
